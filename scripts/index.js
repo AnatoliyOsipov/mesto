9fa2a7bab56245e54cbd1e64c74d15ff0936f7d8
Anatoliy Osipov <a.osipov.tmb@yandex.ru>
2023-04-01 02:42:21 +0300

    fixed mistakes

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,38 @@
 //Константы закрытия, открытия, сохранения попапа
 const popupElement = document.querySelector(".popup");
 const popupCloseButtonElement = popupElement.querySelector(".popup__close");
-const popupOpenButtonElement = document.querySelector(".button__change");
+const popupOpenButtonElement = document.querySelector(".profile__change");
 
 //Присваивание имени и описания на странице
-const userNameElement = document.querySelector(".profile__info-title");
-const userDescriptionElement = document.querySelector(
-  ".profile__info-subtitle"
-);
+const userNameElement = document.querySelector(".profile__title");
+const userDescriptionElement = document.querySelector(".profile__subtitle");
 
 //Присваивание констант инпуту
-const userNameInput = popupElement.querySelector(".popup__name");
-const userDescriptionInput = popupElement.querySelector(".popup__description");
+const userNameInput = popupElement.querySelector(".popup__input_name");
+const userDescriptionInput = popupElement.querySelector(
+  ".popup__input_description"
+);
 
 //Функции открытия и закрытия попапа
 const openPopup = function () {
+  userNameInput.value = userNameElement.textContent;
+  userDescriptionInput.value = userDescriptionElement.textContent;
   popupElement.classList.add("popup_opened");
 };
 const closePopup = function () {
   popupElement.classList.remove("popup_opened");
 };
-const closePopupByClickOnOverlay = function (event) {
+/*const closePopupByClickOnOverlay = function (event) {
   if (event.target !== event.currentTarget) {
     return;
   }
   closePopup();
-};
+};*/
 
 //Слушатели событий на клик
 popupOpenButtonElement.addEventListener("click", openPopup);
 popupCloseButtonElement.addEventListener("click", closePopup);
-popupElement.addEventListener("click", closePopupByClickOnOverlay);
+//popupElement.addEventListener("click", closePopupByClickOnOverlay);
 
 //submit
 const formElement = document.getElementById("edit-form");
